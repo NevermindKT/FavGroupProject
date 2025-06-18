cc18bde543711be759891b8a387339c99ba1c000
NVKT <nevermindktab@gmail.com>
2025-06-18 14:20:21 +0300

    five

--- src/modules/MusicPlayer.jsx
+++ src/modules/MusicPlayer.jsx
@@ -4,42 +4,42 @@ import '/src/CSS/MyCSS.css';
 const tracks = [
     {
         title: 'Fat Lip',
-        src: 'public/songs/fat-lip-sum41.mp3',
+        src: '/songs/fat-lip-sum41.mp3',
         img: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Sum_41_All_Killer_No_Filler.jpg/250px-Sum_41_All_Killer_No_Filler.jpg',
     },
     {
         title: 'Still Waiting',
-        src: 'public/songs/still-waiting.mp4',
+        src: '/songs/still-waiting.mp4',
         img: 'https://upload.wikimedia.org/wikipedia/en/thumb/1/19/Sum41_doesthislookinfected.png/250px-Sum41_doesthislookinfected.png',
     },
     {
         title: 'Pieces',
-        src: 'public/songs/pieces.mp4',
+        src: '/songs/pieces.mp4',
         img: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/49/Sum41chuck.jpg/250px-Sum41chuck.jpg',
     },
     {
         title: 'Walking Disaster',
-        src: 'public/songs/walking-disaster.mp4',
+        src: '/songs/walking-disaster.mp4',
         img: 'https://upload.wikimedia.org/wikipedia/en/thumb/f/f5/SUM_41_UNDERCLASS_HERO.jpg/250px-SUM_41_UNDERCLASS_HERO.jpg',
     },
     {
         title: 'Screaming Bloody Murder',
-        src: 'public/songs/screaming-bloody-murder.mp4',
+        src: '/songs/screaming-bloody-murder.mp4',
         img: 'https://upload.wikimedia.org/wikipedia/en/6/65/Screaming_Bloody_Murder.jpg',
     },
     {
         title: 'Goddamn I\'m Dead Again',
-        src: 'public/songs/goddamn-i\'m-dead-again.mp4',
+        src: '/songs/goddamn-i\'m-dead-again.mp4',
         img: 'https://upload.wikimedia.org/wikipedia/en/7/71/13_Voices_album_artwork.jpg',
     },
     {
         title: '45 (A Matter Of Time)',
-        src: 'public/songs/45(a-matter-of-time).mp4',
+        src: '/songs/45(a-matter-of-time).mp4',
         img: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Order_in_decline_sum_41.png/250px-Order_in_decline_sum_41.png',
     },
     {
         title: 'Landmines',
-        src: 'public/songs/landmines.mp4',
+        src: '/songs/landmines.mp4',
         img: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/56/Sum_41_-_Heaven_x_Hell.png/250px-Sum_41_-_Heaven_x_Hell.png',
     },
 ];
